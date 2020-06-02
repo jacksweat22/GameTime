38f6d584f8ad895a0df2bab563e74f3153d921a2
Jack Sweat <jacksweat@jacks-mbp.lan>
2020-06-02 11:35:01 -0700

    components developed, render formed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Game-Time
-    </div>
-  );
+class App extends Component {
+
+render() {
+    return (
+      <>
+      <div className="header">
+        <header>
+
+       </header>
+      </div>
+    </>
+    );
+  }
 }
+
 
 export default App;
